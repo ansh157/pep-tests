1aae64c3a93a1c33f0c640b400552140f46bdb9c
Ansh Singh <anshsingh.x1234@gmail.com>
2026-02-04 10:09:10 +0530

    updated

--- test1/script.js
+++ test1/script.js
@@ -1,4 +1,4 @@
-let emoji = ["😀","😂","😍","🤩","😎","🥳","🤨","🫨","🥵"];
+let emoji = ["😀","😂","😍","🤩","😎","🥳","🤨","🫨","🥵","💀","👻","🤖","🤕","👺","😈"];
 let btn = document.querySelector('.gen-btn');
 btn.addEventListener('click', generateEmoji);
 
@@ -6,12 +6,25 @@ let emojiDisplay = document.querySelector('.emoji-display');
 
 let randomColor = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
 let color = "#";
+let emojiArray = document.querySelector('.emoji-array');
 
 function generateEmoji() {
     emojiIndex = Math.floor(Math.random() * emoji.length);
     emojiDisplay.textContent = emoji[emojiIndex];
     let box = document.querySelector('.mystery-box');
-    box.style.transform = "rotateY(180deg)";
+
+    let emojiDiv = document.createElement('div');
+    emojiDiv.innerText = emoji[emojiIndex];
+    emojiDiv.style.fontSize = "2rem";
+    emojiArray.appendChild(emojiDiv);
+
+    emojiDiv.addEventListener('click', function(){
+        navigator.clipboard.writeText(emojiDiv.innerText);
+        alert("Emoji " + emojiDiv.innerText + " copied to clipboard!");
+    });
+
+    box.style.animation = "flip 0.5s ease-in-out";
+    // box.style.transform = "rotateY(180deg)";
     box.style.backgroundColor = "red";
     let card = document.querySelector('.card');
     card.style.backgroundColor = generateRandomColor();
